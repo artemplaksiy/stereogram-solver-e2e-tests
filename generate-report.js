1fc78432f9aa9dae949485b2078c1d55814151cd
Artem Plaksiy <artem.plaksiy@aevi.com>
2025-06-02 09:52:45 +0200

    Reporter format fixes

--- generate-report.js
+++ generate-report.js
@@ -2,5 +2,23 @@ const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
   jsonDir: "cypress/reports/",
-  reportPath: "cypress/reports/html"
+  reportPath: "cypress/reports/html",
+  metadata: {
+    browser: {
+      name: "Chrome",
+      version: "123",
+    },
+    device: "Local test machine",
+    platform: {
+      name: process.platform,
+      version: "latest",
+    },
+  },
+  customData: {
+    title: "Run Info",
+    data: [
+      { label: "Project", value: "Stereogram Solver E2E Tests" },
+      { label: "Execution Time", value: new Date().toLocaleString() }
+    ]
+  }
 });
